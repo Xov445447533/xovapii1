8b7bb05b04024646dd67998454363746db4fb508
✮ 𝑽𝒗 𝑬𝒆 𝑵𝒏 𝑶𝒐 𝑴 ✮ <153835525+Xov445447533@users.noreply.github.com>
2024-07-14 23:47:32 +0300

    Update loading.js

--- xov_js/loading.js
+++ xov_js/loading.js
@@ -1,1 +1,59 @@
-.
+(function($) { 
+    $.fn.glitch = function(options) {
+        const defaults = {
+            chars: '!<>-_\\/[]{}—=+*^?#________',
+            charTime: 50,
+            finalText: undefined,
+            done: function() {
+                console.log('done!');
+            }
+        };
+        let settings = $.extend({}, defaults, options),
+            $element = $(this),
+            originalText = $element.text(),
+            scrambledText = '';
+
+        function randomChar() {
+            return settings.chars[Math.floor(Math.random() * settings.chars.length)];
+        }
+
+        function scrambleText(text, index, char) {
+            if (index > text.length - 1) return text;
+            return text.substr(0, index) + char + text.substr(index + 1);
+        }
+
+        function scramble(index) {
+            let deferred = $.Deferred(),
+                delay = Math.floor(Math.random() * (1000 - 100) + 100),
+                interval = Math.floor(Math.random() * settings.charTime);
+
+            let intervalId = setInterval(function() {
+                if (interval <= 0) {
+                    clearInterval(intervalId);
+                    deferred.resolve();
+                    $element.text(scrambleText($element.text(), index, settings.finalText.charAt(index)));
+                } else {
+                    $element.text(scrambleText($element.text(), index, randomChar()));
+                    interval--;
+                }
+            }, delay);
+
+            return deferred.promise();
+        }
+
+        $element.text(new Array(originalText.length + 1).join(''));
+
+        let promises = [];
+        for (let i = 0; i < originalText.length; i++) {
+            promises.push(scramble(i));
+        }
+
+        $.when.apply($, promises).then(function() {
+            settings.done($element);
+        });
+
+        return this;
+    };
+})(jQuery);
+
+$('#header').glitch({ charTime: 60 });
